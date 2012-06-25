ea64ec61781dba35cf6ece44e57ca221acf94cb0
Keith Trnka <keith.trnka@gmail.com>
2012-06-24 22:56:20 -0400

    Refactored HomeActivity to use ListView better.

--- gen/edu/udel/trnka/pl/R.java
+++ gen/edu/udel/trnka/pl/R.java
@@ -14,29 +14,24 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int LinearLayout1=0x7f050001;
-        public static final int aboutTextView=0x7f050004;
         public static final int about_credits=0x7f050000;
-        public static final int contactText=0x7f05000a;
-        public static final int interpersonalTextView=0x7f050003;
-        public static final int personalTextView=0x7f050002;
-        public static final int phraseText=0x7f050008;
-        public static final int runtimeText=0x7f05000e;
-        public static final int scrollView1=0x7f050006;
-        public static final int statText=0x7f05000c;
-        public static final int testList=0x7f05000f;
-        public static final int textView1=0x7f050005;
-        public static final int textView2=0x7f050007;
-        public static final int textView3=0x7f050009;
-        public static final int textView5=0x7f05000b;
-        public static final int textView6=0x7f05000d;
+        public static final int contactText=0x7f050006;
+        public static final int phraseText=0x7f050004;
+        public static final int runtimeText=0x7f05000a;
+        public static final int scrollView1=0x7f050002;
+        public static final int statText=0x7f050008;
+        public static final int testList=0x7f05000b;
+        public static final int textView1=0x7f050001;
+        public static final int textView2=0x7f050003;
+        public static final int textView3=0x7f050005;
+        public static final int textView5=0x7f050007;
+        public static final int textView6=0x7f050009;
     }
     public static final class layout {
         public static final int about=0x7f030000;
-        public static final int home=0x7f030001;
-        public static final int interpersonal=0x7f030002;
-        public static final int personal_basic=0x7f030003;
-        public static final int test=0x7f030004;
+        public static final int interpersonal=0x7f030001;
+        public static final int personal_basic=0x7f030002;
+        public static final int test=0x7f030003;
     }
     public static final class string {
         public static final int about=0x7f040001;
--- src/edu/udel/trnka/pl/HomeActivity.java
+++ src/edu/udel/trnka/pl/HomeActivity.java
@@ -1,54 +1,77 @@
 package edu.udel.trnka.pl;
 
-import edu.udel.trnka.pl.R;
-import android.app.Activity;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
 
-public class HomeActivity extends Activity
+public class HomeActivity extends ListActivity
 	{
-	/** Called when the activity is first created. */
-	@Override
+	// TODO: use strings.xml for localization
+	private static final String[] names = { "Personal Stats", "Interpersonal Stats", "Send Feedback", "About" };
+	private static final String[] descriptions = { "Analyse sent messages.", "Compare your messages to messages from your friends!", "", "" };
+	
+	// TODO:  I don't like how this uses parallel arrays.  I'd much rather do something like make an instance that has all this (could I do it by overriding toString in the Acitivites?)
+	private static final Class<?>[] activities = { PersonalLingActivity.class, null, null, AboutActivity.class };
+	
 	public void onCreate(Bundle savedInstanceState)
 		{
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.home);
 		
-		final TextView personalText = (TextView) findViewById(R.id.personalTextView);
-		personalText.setOnClickListener(new View.OnClickListener()
+		// build the structure we need to pass to SimpleAdapter
+		ArrayList<HashMap<String,String>> fields = new ArrayList<HashMap<String,String>>();
+		for (int i = 0; i < names.length; i++)
 			{
-				
-				public void onClick(View v)
-					{
-					Intent intent = new Intent(HomeActivity.this, PersonalLingActivity.class);
-					startActivity(intent);
-					}
-			});
+			HashMap<String,String> item = new HashMap<String,String>();
+			item.put("name", names[i]);
+			item.put("desc", descriptions[i]);
+			fields.add(item);
+			}
 		
-		final TextView aboutText = (TextView) findViewById(R.id.aboutTextView);
-		aboutText.setOnClickListener(new View.OnClickListener()
-			{
-				
-				public void onClick(View v)
-					{
-					Intent intent = new Intent(HomeActivity.this, TestActivity.class);
-					startActivity(intent);
-					}
-			});
+		// simple adapter that's just plain strings
+		//ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+		//		android.R.layout.simple_list_item_1, android.R.id.text1, names);
+		
+		SimpleAdapter adapter = new SimpleAdapter(this, fields,
+				android.R.layout.simple_list_item_2,
+				new String[] { "name", "desc" },
+				new int[] { android.R.id.text1, android.R.id.text2 });
+		
+		
+		setListAdapter(adapter);
+		}
 
-		final TextView interpersonalText = (TextView) findViewById(R.id.interpersonalTextView);
-		interpersonalText.setOnClickListener(new View.OnClickListener()
+	public void onListItemClick(ListView list, View view, int position, long id)
+		{
+		if (position < activities.length && activities[position] != null)
 			{
-				
-				public void onClick(View v)
-					{
-					Intent intent = new Intent(HomeActivity.this, InterpersonalActivity.class);
-					startActivity(intent);
-					}
-			});
+			// launch the activity if it's found
+			Intent intent = new Intent(this, activities[position]);
+			startActivity(intent);
+			}
+		else if (position < names.length && names[position].equals("Send Feedback"))
+			{
+			// special case for the feedback option
+			
+			Intent sendIntent = new Intent(Intent.ACTION_SEND);
+			sendIntent.setType("message/rfc822");
+			sendIntent.putExtra(Intent.EXTRA_EMAIL, new String[] { "keith.trnka@gmail.com" });
+			sendIntent.putExtra(Intent.EXTRA_SUBJECT, "Feedback on " + getString(R.string.app_name));
+			
+			startActivity(Intent.createChooser(sendIntent, "Send email using..."));
+			}
+		else
+			{
+			Toast.makeText(getApplicationContext(), "Not Implemented Yet", Toast.LENGTH_SHORT).show();
+			}
 
+	
 		}
 
 
