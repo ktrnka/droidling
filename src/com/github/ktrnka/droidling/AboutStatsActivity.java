080bdc8a9b16223c71ef1599d3c7684eab2f93bb
Keith Trnka <keith.trnka@gmail.com>
2012-08-27 22:36:42 -0400

    Adding missing files.

---
+++ src/com/github/ktrnka/droidling/AboutStatsActivity.java
@@ -0,0 +1,13 @@
+package com.github.ktrnka.droidling;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AboutStatsActivity extends Activity
+	{
+    public void onCreate(Bundle savedInstanceState)
+		{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.about_stats);
+		}
+	}
