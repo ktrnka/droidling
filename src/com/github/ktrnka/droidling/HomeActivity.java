674d379a8321676774ed718bc429a9211872827b
Keith Trnka <trnka.dev@gmail.com>
2013-01-12 11:42:24 -0800

    Added What's New popup on first install or update
    It works right, but needs to do the threading right.

--- src/com/github/ktrnka/droidling/HomeActivity.java
+++ src/com/github/ktrnka/droidling/HomeActivity.java
@@ -1,14 +1,23 @@
 package com.github.ktrnka.droidling;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 import com.github.ktrnka.droidling.R;
 
+import android.app.AlertDialog;
 import android.app.ListActivity;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -24,12 +33,37 @@ public class HomeActivity extends ListActivity
 	
 	public static final boolean DEVELOPER_MODE = true;
 	
+	public static final String TAG = "com.github.ktrnka.droidling";
+	
 	public void onCreate(Bundle savedInstanceState)
 		{
 		super.onCreate(savedInstanceState);
 		
 		// I don't like this code AT ALL, but getString is an instance method :(
 		descriptions[3] = "Send email to " + getString(R.string.developer_email);
+		
+		try
+			{
+			PackageInfo packageInfo = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_META_DATA);
+			SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+			if (prefs.getLong("lastRunVersionCode", 0) < packageInfo.versionCode)
+				{
+				showWelcome();
+				
+				// update the preference
+				SharedPreferences.Editor editor = prefs.edit();
+				editor.putLong("lastRunVersionCode", packageInfo.versionCode);
+				editor.commit();
+				}
+			}
+		catch (PackageManager.NameNotFoundException exc)
+			{
+			// This error shouldn't be possible; I don't know an accurate way to test it.
+			// If the PackageManager is broken somehow, we probably shouldn't show What's New every time.
+			// Should we show a warning?  I don't know.
+			Log.e(TAG, "PackageManager lookup failed");
+			Log.e(TAG, Log.getStackTraceString(exc));
+			}
 		
 		// build the structure we need to pass to SimpleAdapter
 		ArrayList<HashMap<String,String>> fields = new ArrayList<HashMap<String,String>>();
@@ -42,6 +76,36 @@ public class HomeActivity extends ListActivity
 			}
 		
 		setListAdapter(new DescriptionMenuAdapter(this, names, descriptions));
+		}
+
+	private void showWelcome()
+		{
+		try 
+			{
+			// try loading the changelog file
+			StringBuilder changeLogBuilder = new StringBuilder();
+			BufferedReader in = new BufferedReader(new InputStreamReader(getAssets().open("changelog.txt")));
+			String line;
+			while ( (line = in.readLine()) != null)
+				{
+				changeLogBuilder.append(line);
+				changeLogBuilder.append("\n");
+				}
+			in.close();
+		
+			AlertDialog.Builder alertBuilder = new AlertDialog.Builder(this);
+			alertBuilder.setTitle("What's New");
+			alertBuilder.setMessage(changeLogBuilder);
+			
+			alertBuilder.setPositiveButton("Close", null);
+			alertBuilder.show();
+
+			} 
+		catch (IOException e)
+			{
+			Log.e(TAG, "Failure to load changelog");
+			Log.e(TAG, Log.getStackTraceString(e));
+			}
 		}
 
 	public void onListItemClick(ListView list, View view, int position, long id)
