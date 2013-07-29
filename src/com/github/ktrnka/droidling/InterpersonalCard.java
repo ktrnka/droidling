ea1e49bf69dff7f82db94f6fce97b081abc12add
Keith Trnka <trnka.dev@gmail.com>
2013-07-28 17:59:36 -0700

    Updated cards for view recycling
    Goes along with the changes in the cards project to allow view
    recycling.  Solves performance issues when scrolling.

--- src/com/github/ktrnka/droidling/InterpersonalCard.java
+++ src/com/github/ktrnka/droidling/InterpersonalCard.java
@@ -10,30 +10,31 @@ import android.widget.TextView;
 
 import com.fima.cardsui.objects.Card;
 
-public class InterpersonalCard extends Card {
-	// TODO: Investigate whether this will cause memory leaks by preventing GC of the activity.
+public class InterpersonalCard extends Card
+	{
+	// TODO: Investigate whether this will cause memory leaks by preventing GC
+	// of the activity.
 	private Context activityContext;
 	private InterpersonalSingleStats stats;
-	
-	public InterpersonalCard(String title, InterpersonalSingleStats stats, Context activityContext) {
+
+	public InterpersonalCard(String title, InterpersonalSingleStats stats, Context activityContext)
+		{
 		super(title, null, 0);
-		
+
 		this.activityContext = activityContext;
-		
+
 		this.stats = stats;
-	}
+		}
+
+	public int getCardContentId()
+		{
+		return R.layout.card_interpersonal2;
+		}
 
 	@Override
-	public View getCardContent(Context context) {
-	/*
-		View view = LayoutInflater.from(context).inflate(R.layout.card_ex, null);
-	
-		((TextView) view.findViewById(R.id.title)).setText(title);
-		((TextView) view.findViewById(R.id.description)).setText(stats.numSentText);
-	
-		return view;
-		*/
-		View view = LayoutInflater.from(context).inflate(R.layout.card_interpersonal2, null);
+	public View getCardContent(final Context context)
+		{
+		View view = LayoutInflater.from(context).inflate(getCardContentId(), null);
 
 		((TextView) view.findViewById(R.id.title)).setText(title);
 		((TextView) view.findViewById(R.id.numSent)).setText(stats.numSentText);
@@ -43,7 +44,7 @@ public class InterpersonalCard extends Card {
 		((TextView) view.findViewById(R.id.responseTime)).setText(stats.responseTimeText);
 		((TextView) view.findViewById(R.id.bigramGeneration)).setText(stats.bigramGenerationText);
 		((TextView) view.findViewById(R.id.trigramGeneration)).setText(stats.trigramGenerationText);
-		
+
 		ImageView shareView = (ImageView) view.findViewById(R.id.share);
 		shareView.setOnClickListener(new View.OnClickListener()
 			{
@@ -51,26 +52,66 @@ public class InterpersonalCard extends Card {
 				{
 				if (activityContext == null)
 					return;
-				
+
 				String subject = "Shared stats from " + activityContext.getString(R.string.app_name);
-				// TODO: There's no longer a valid single desc field
-				String text = "Stats: " + title + ":\n" + desc;
-				
+				String text = "Stats: " + title + ":\n" + stats.toString(context);
+
 				Intent sendIntent = new Intent(Intent.ACTION_SEND);
 				sendIntent.setType("text/plain");
 				sendIntent.putExtra(Intent.EXTRA_TEXT, text);
 				sendIntent.putExtra(Intent.EXTRA_SUBJECT, subject);
-				
+
 				activityContext.startActivity(Intent.createChooser(sendIntent, activityContext.getString(R.string.share_intent)));
 				}
 			});
-		
+
 		Log.d("com.github.droidling.InterpersonalCard", "Inflated card for " + title);
 
 		return view;
-	}
+		}
 
-	
-	
-	
-}
+	@Override
+    public boolean convert(final View convertCardView)
+	    {
+	    View view = convertCardView.findViewById(R.id.cardContentRoot);
+	    if (view == null)
+	    	{
+	    	Log.d("com.github.ktrnka.droidling.InterpersonalCard", "Can't find card content root");
+	    	return false;
+	    	}
+	    
+		((TextView) view.findViewById(R.id.title)).setText(title);
+		((TextView) view.findViewById(R.id.numSent)).setText(stats.numSentText);
+		((TextView) view.findViewById(R.id.averageMessageLength)).setText(stats.messageLengthText);
+		((TextView) view.findViewById(R.id.sharedVocab)).setText(stats.sharedVocabPercentText);
+		((TextView) view.findViewById(R.id.sharedPhrases)).setText(stats.sharedPhrasesText);
+		((TextView) view.findViewById(R.id.responseTime)).setText(stats.responseTimeText);
+		((TextView) view.findViewById(R.id.bigramGeneration)).setText(stats.bigramGenerationText);
+		((TextView) view.findViewById(R.id.trigramGeneration)).setText(stats.trigramGenerationText);
+
+		ImageView shareView = (ImageView) view.findViewById(R.id.share);
+		shareView.setOnClickListener(new View.OnClickListener()
+			{
+			public void onClick(View v)
+				{
+				if (activityContext == null)
+					return;
+
+				String subject = "Shared stats from " + activityContext.getString(R.string.app_name);
+				String text = "Stats: " + title + ":\n" + stats.toString(convertCardView.getContext());
+
+				Intent sendIntent = new Intent(Intent.ACTION_SEND);
+				sendIntent.setType("text/plain");
+				sendIntent.putExtra(Intent.EXTRA_TEXT, text);
+				sendIntent.putExtra(Intent.EXTRA_SUBJECT, subject);
+
+				activityContext.startActivity(Intent.createChooser(sendIntent, activityContext.getString(R.string.share_intent)));
+				}
+			});
+
+		Log.d("com.github.droidling.InterpersonalCard", "Reused card for " + title);
+
+	    return true;
+	    }
+
+	}
--- src/com/github/ktrnka/droidling/TitledCard.java
+++ src/com/github/ktrnka/droidling/TitledCard.java
@@ -7,27 +7,45 @@ import android.widget.TextView;
 
 import com.fima.cardsui.objects.Card;
 
-public class TitledCard extends Card {
-
-	public TitledCard(String title){
+public class TitledCard extends Card
+	{
+	public TitledCard(String title)
+		{
 		super(title);
-	}
-	
-	public TitledCard(String title, String text) {
+		}
+
+	public TitledCard(String title, String text)
+		{
 		super(title, text, 0);
-	}
+		}
 
 	@Override
-	public View getCardContent(Context context) {
-		View view = LayoutInflater.from(context).inflate(R.layout.card_ex, null);
+	public View getCardContent(Context context)
+		{
+		View view = LayoutInflater.from(context).inflate(getCardContentId(), null);
 
 		((TextView) view.findViewById(R.id.title)).setText(title);
 		((TextView) view.findViewById(R.id.description)).setText(desc);
 
 		return view;
-	}
+		}
 
-	
-	
-	
-}
+	@Override
+	public int getCardContentId()
+		{
+		return R.layout.card_ex;
+		}
+
+	@Override
+    public boolean convert(View convertCardView)
+	    {
+	    View view = convertCardView.findViewById(getCardContentId());
+	    if (view == null)
+	    	return false;
+	    
+		((TextView) view.findViewById(R.id.title)).setText(title);
+		((TextView) view.findViewById(R.id.description)).setText(desc);
+	    
+	    return true;
+	    }
+	}
